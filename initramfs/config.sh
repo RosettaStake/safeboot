a362200affa1b15e74e4d7ea61a75a34e963ca4b
epeus <4056189+anhnd@users.noreply.github.com>
2021-08-15 16:33:42 +0700

    Minimal version

--- initramfs/config.sh
+++
@@ -1,6 +0,0 @@
-# Configuration for qemu machines
-export ETH_DEV="eth0"
-export ATTEST_SERVER="10.0.2.2"
-export ATTEST_URL="http://$ATTEST_SERVER:8080/attest"
-export ROOTFS_URL="http://$ATTEST_SERVER:8080/focal-server-cloudimg-amd64.img"
-export ROOTFS_DEV="/dev/sda"
